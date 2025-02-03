422b5bb9ecc32ca211d6a5f93916afa518c9ff7b
AlpacaMale <alpaca9100@gmail.com>
2025-02-03 20:29:33 +0900

    test: update api test

--- server/request.py
+++ server/request.py
@@ -7,8 +7,13 @@ url = "http://127.0.0.1:5000"
 headers = {"Content-Type": "application/json; charset=utf-8"}
 
 
-def get_menu():
-    response = requests.get(f"{url}/menu", headers=headers)
+def get_menus():
+    response = requests.get(f"{url}/menus", headers=headers)
+    print(response.text)
+
+
+def get_menu(menu_id):
+    response = requests.get(f"{url}/menus/{menu_id}", headers=headers)
     print(response.text)
 
 
@@ -21,7 +26,7 @@ def post_menu(name, name_en, kind, base_price, type, img_path):
         "type": type,
         "img_path": img_path,
     }
-    response = requests.post(f"{url}/menu", headers=headers, data=json.dumps(data))
+    response = requests.post(f"{url}/menus", headers=headers, data=json.dumps(data))
     print(response.text)
 
 
@@ -33,29 +38,30 @@ def update_menu():
         "base_price": 3000,
         "img_path": "./img/espresso",
     }
-    response = requests.put(f"{url}/menu/1", headers=headers, data=json.dumps(data))
+    response = requests.put(f"{url}/menus/1", headers=headers, data=json.dumps(data))
     print(response.text)
 
 
 def delete_menu():
-    response = requests.delete(f"{url}/menu/2", headers=headers)
+    response = requests.delete(f"{url}/menus/2", headers=headers)
     print(response.text)
 
 
 def get_img():
-    response = requests.get(f"{url}/img/1?width=300&height=200", headers=headers)
+    response = requests.get(f"{url}/images/1?width=300&height=200", headers=headers)
     print(response.text)
 
 
+get_menu(1)
 # get_img()
 
 
-with open("server/coffee.csv", "r", encoding="utf-8") as file:
-    for line in file:
-        name, name_en, kind, type, img_path = line.split(",")
-        if name == "name":
-            continue
-        img_path = img_path.strip()
-        base_price = random.choice([3000, 4000, 4500, 5500])
-        # print(base_price)
-        post_menu(name, name_en, kind, base_price, type, img_path)
+# with open("server/coffee.csv", "r", encoding="utf-8") as file:
+#     for line in file:
+#         name, name_en, kind, type, img_path = line.split(",")
+#         if name == "name":
+#             continue
+#         img_path = img_path.strip()
+#         base_price = random.choice([3000, 4000, 4500, 5500])
+#         # print(base_price)
+#         post_menu(name, name_en, kind, base_price, type, img_path)
