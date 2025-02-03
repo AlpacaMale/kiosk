b00f9d7086480b0a67618d95d48997bd8c860f4f
AlpacaMale <alpaca9100@gmail.com>
2025-02-04 07:24:47 +0900

    fix: proper usage of wrapper

--- server/function.py
+++ server/function.py
@@ -1,7 +1,9 @@
+from functools import wraps
 from flask import session, redirect
 
 
 def login_required(f):
+    @wraps(f)
     def wrapper(*args, **kwargs):
         if not session.get("email"):
             return redirect("/admin/login")
